c172a0df31314e9c3aef4cf8be51da56a156a7f6
Eurico Martins <euricomartinss@gmail.com>
2024-11-15 18:42:00 +0000

    Updated file path

--- assessment/main.py
+++ assessment/main.py
@@ -6,7 +6,7 @@ from csp_solver import csp_solver
 
 # Load set the file path, use the input to define the name
 # file_path = input("Enter the path path") or "p01_dataset_8.txt";
-file_path = "assessment/p01_dataset_8.txt"
+file_path = "p01_dataset_8.txt"
 print(os.getcwd())
 
 with open(file_path, "r") as file:
